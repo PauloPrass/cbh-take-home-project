4d6067975bbfa5c48049e22475c4827056f33d36
PauloPrass <pauloricardo.prass@gmail.com>
2023-02-17 14:25:01 -0300

    Code refactorings and implementation of unit tests with Jest.

--- dpk.js
+++ dpk.js
@@ -5,24 +5,22 @@ exports.deterministicPartitionKey = (event) => {
   const MAX_PARTITION_KEY_LENGTH = 256;
   let candidate;
 
-  if (event) {
-    if (event.partitionKey) {
+  try{
+    if (event?.partitionKey) {
       candidate = event.partitionKey;
-    } else {
+    } else if (event){
       const data = JSON.stringify(event);
       candidate = crypto.createHash("sha3-512").update(data).digest("hex");
     }
-  }
 
-  if (candidate) {
-    if (typeof candidate !== "string") {
-      candidate = JSON.stringify(candidate);
+    candidate = candidate ? typeof candidate !== "string" ? candidate = JSON.stringify(candidate) : candidate : candidate = TRIVIAL_PARTITION_KEY;
+
+    if (candidate.length > MAX_PARTITION_KEY_LENGTH) {
+      candidate = crypto.createHash("sha3-512").update(candidate).digest("hex");
     }
-  } else {
-    candidate = TRIVIAL_PARTITION_KEY;
+    return candidate;
+  } catch(error) {
+    console.log("An error occurred in the method deterministicPartitionKey: "+ error);
+    return "0";
   }
-  if (candidate.length > MAX_PARTITION_KEY_LENGTH) {
-    candidate = crypto.createHash("sha3-512").update(candidate).digest("hex");
-  }
-  return candidate;
 };
